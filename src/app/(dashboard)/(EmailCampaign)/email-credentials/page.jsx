2e478801fb556d9924e61b9f7c808ae604b05db2
Satya Adabala <satya1adabala@gmail.com>
2025-08-27 22:52:20 +0530

    update

--- src/app/(dashboard)/(EmailCampaign)/email-credentials/page.jsx
+++ src/app/(dashboard)/(EmailCampaign)/email-credentials/page.jsx
@@ -63,7 +63,7 @@ const EmailCredentials = () => {
   useEffect(() => {
     const userData = getUserData();
     if (!userData || !userData.pic) {
-      router.push("/work/login");
+      router.push("/login");
     } else {
       setUserID(userData.userID);
       fetchData(userData.userID);
